636f09df20d7e9e21387385423468c006e5167df
vkaelin <kalanehd@gmail.com>
2019-11-19 11:43:13 +0100

    feat: add axios/tailwindcss/purgecss/eslint to vue project

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import VueAxios from './plugins/axios'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/assets/css/main.css'
+
 Vue.config.productionTip = false
+Vue.use(VueAxios)
 
 new Vue({
   router,
