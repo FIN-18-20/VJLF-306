e66639d8d0d3d9b52e2b3bc1e3480b06425d9d6e
vkaelin <kalanehd@gmail.com>
2019-11-26 16:21:43 +0100

    feat: sort by brand now works

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,59 +3,132 @@
     <div class="PrintersNav">
       <div class="text-center text-3xl">Trier par :</div>
       <div class="buttonRow1 flex justify-center content-around mt-2">
-        <button class="brand bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Marques
-        </button>
-        <button class="Height ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Volumes
-        </button>
-        <button class="weight ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Poids
-        </button>
-        <button class="priceEvolution ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Évolution du prix
-        </button>
-        <button class="constructor ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Constructeurs
-        </button>
-        <button class="bestSales ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Meilleures ventes
-        </button>
-        <button class="impressionSpeed ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Vitesse d'impression
-        </button>
+        <button
+          @click="callAPI('brand')"
+          class="brand bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Marques</button>
+        <button
+          @click="callAPI('size')"
+          class="Height ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Volumes</button>
+        <button
+          @click="callAPI('weight')"
+          class="weight ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Poids</button>
+        <button
+          @click="callAPI('price')"
+          class="priceEvolution ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Évolution du prix</button>
+        <button
+          @click="callAPI('constructor')"
+          class="constructor ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Constructeurs</button>
+        <button
+          @click="callAPI('sell')"
+          class="bestSales ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Meilleures ventes</button>
+        <button
+          @click="callAPI('speed')"
+          class="impressionSpeed ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Vitesse d'impression</button>
       </div>
       <div class="buttonRow2 flex justify-center content-around mt-2">
-        <button class="resolution ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Résolutions
-        </button>
-        <button class="extremePrices ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Prix extrêmes
-        </button>
-        <button class="constructorAndPrice ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Constructeurs & prix 
-        </button>
-        <button class="PrintersConsumable ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Imprimantes et consommables 
-        </button>
-        <button class="Consumable ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-          Consommables
-        </button>
+        <button
+          @click="callAPI('resolution')"
+          class="resolution ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Résolutions</button>
+        <button
+          @click="callAPI('price')"
+          class="extremePrices ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Prix extrêmes</button>
+        <button
+          @click="todo()"
+          class="constructorAndPrice ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Constructeurs & prix</button>
+        <button
+          @click="todo()"
+          class="PrintersConsumable ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Imprimantes et consommables</button>
+        <button
+          @click="todo()"
+          class="Consumable ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >Consommables</button>
       </div>
     </div>
-    <div class="PrintersInfo mt-24 p-64 mx-auto border-gray-600 border-double border-8 text-center">
-      C'est la qu'on met les infos demandées avec un lien sur la page détails avec toutes les infos
+    <div
+      class="PrintersInfo mt-24 px-16 py-8 mx-auto border-gray-600 border-double border-8 text-center"
+    >
+      <div v-show="result.length">
+        <table class="table-auto w-full mx-auto">
+          <thead>
+            <tr>
+              <th v-for="printer in displayKeys" :key="printer.Id" class="px-4 py-2">{{ printer }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(printer, index) in result"
+              :key="printer['show-Id']"
+              :class="{'bg-gray-100': index % 2 === 0}"
+            >
+              <td
+                v-for="key in displayKeys"
+                :key="printer['show-Id'] + key"
+                class="border px-4 py-2"
+              >{{ printer[`show-${key}`] }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-show="!result.length">
+        <p>Veuillez choisir votre filtre ci-dessus afin d'afficher la liste d'imprimantes.</p>
+      </div>
     </div>
-    
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      result: []
+    }
+  },
+
+  computed: {
+    displayKeys() {
+      if (!this.result.length) {
+        return null
+      }
+
+      return Object.keys(this.result[0])
+        .filter(key => key.startsWith('show-'))
+        .reduce((array, key) => {
+          return [
+            ...array,
+            key.split('show-')[1]
+          ]
+        }, [])
+    }
+  },
+
+  methods: {
+    async callAPI(url) {
+      try {
+        const result = await this.$axios(({ url: url, method: 'GET' }))
+        console.log(result.data)
+        this.result = result.data
+        console.log(this.displayKeys)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    todo() {
+      alert('Bouton à implémenter.')
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 </style>
--- server/app/Controllers/Http/PrinterController.js
+++ server/app/Controllers/Http/PrinterController.js
@@ -11,11 +11,15 @@ class PrinterController {
 
   async brand() {
     const result = await Database.raw(`
-      SELECT DISTINCT t_printers.idPrinter as 'show-Id', t_printers.priName as 'show-Nom', t_printers.priSales as 'show-Ventes', t_printers.idBrands,
+      SELECT t_printers.idPrinter as 'show-Id', t_printers.priName as 'show-Nom', t_printers.priSales as 'show-Ventes', t_printers.idBrands,
       t_prices.priValue as 'show-Prix', t_prices.priDate, t_brands.braName as 'show-Marque', t_brands.idConstructor
       FROM t_printers
       INNER JOIN t_brands ON t_brands.idBrands = t_printers.idBrands
       INNER JOIN t_prices ON t_prices.idPrinter = t_printers.idPrinter
+      WHERE t_prices.idPrice IN (
+          SELECT MAX(t_prices.idPrice) FROM t_prices
+          GROUP BY t_prices.idPrinter
+      )
       ORDER BY t_prices.priDate DESC, t_brands.braName
     `)
     return result[0]
