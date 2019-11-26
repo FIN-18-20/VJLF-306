f3eee2cb35786a552107811d93acdaf06598fcf2
Marinlestylo <chil.tfm@gmail.com>
2019-11-26 11:26:54 +0100

    feat: finished home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,46 +1,52 @@
 <template>
   <div class="home container mx-auto">
-    <div class="text-center text-3xl">Trier par :</div>
-    <div class="buttonRow1 flex justify-center content-around mt-2">
-      <button class="brand bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Marques
-      </button>
-      <button class="Height ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Volumes
-      </button>
-      <button class="weight ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Poids
-      </button>
-      <button class="priceEvolution ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Évolution du prix
-      </button>
-      <button class="constructor ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Constructeurs
-      </button>
-      <button class="bestSales ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Meilleures ventes
-      </button>
-      <button class="impressionSpeed ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Vitesse d'impression
-      </button>
+    <div class="PrintersNav">
+      <div class="text-center text-3xl">Trier par :</div>
+      <div class="buttonRow1 flex justify-center content-around mt-2">
+        <button class="brand bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Marques
+        </button>
+        <button class="Height ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Volumes
+        </button>
+        <button class="weight ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Poids
+        </button>
+        <button class="priceEvolution ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Évolution du prix
+        </button>
+        <button class="constructor ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Constructeurs
+        </button>
+        <button class="bestSales ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Meilleures ventes
+        </button>
+        <button class="impressionSpeed ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Vitesse d'impression
+        </button>
+      </div>
+      <div class="buttonRow2 flex justify-center content-around mt-2">
+        <button class="resolution ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Résolutions
+        </button>
+        <button class="extremePrices ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Prix extrêmes
+        </button>
+        <button class="constructorAndPrice ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Constructeurs & prix 
+        </button>
+        <button class="PrintersConsumable ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Imprimantes et consommables 
+        </button>
+        <button class="Consumable ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          Consommables
+        </button>
+      </div>
     </div>
-    <div class="buttonRow1 flex justify-center content-around mt-2">
-      <button class="resolution ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Résolutions
-      </button>
-      <button class="extremePrices ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Prix extrêmes
-      </button>
-      <button class="constructorAndPrice ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Constructeurs & prix 
-      </button>
-      <button class="PrintersConsumable ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Imprimantes et consommables 
-      </button>
-      <button class="Consumable ml-2 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        Consommables
-      </button>
+    <div class="PrintersInfo mt-24 p-64 mx-auto border-gray-600 border-double border-8 text-center">
+      C'est la qu'on met les infos demandées avec un lien sur la page détails avec toutes les infos
     </div>
+    
   </div>
 </template>
 
