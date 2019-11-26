fd62de8c1296eb700d710da9d90217f4d4e77ba5
vkaelin <kalanehd@gmail.com>
2019-11-26 12:09:18 +0100

    feat: create routes and add sql query to sort by brand

--- server/app/Controllers/Http/PrinterController.js
+++ server/app/Controllers/Http/PrinterController.js
@@ -5,8 +5,20 @@ const Database = use('Database')
 class PrinterController {
   async test() {
     // const result = await Database.raw('select * from users where username = ?', [username])
-    const result = await Database.raw('select * from t_constructors')[0]
-    return result
+    const result = await Database.raw('select * from t_printers')
+    return result[0]
+  }
+
+  async brand() {
+    const result = await Database.raw(`
+      SELECT DISTINCT t_printers.idPrinter as 'show-Id', t_printers.priName as 'show-Nom', t_printers.priSales as 'show-Ventes', t_printers.idBrands,
+      t_prices.priValue as 'show-Prix', t_prices.priDate, t_brands.braName as 'show-Marque', t_brands.idConstructor
+      FROM t_printers
+      INNER JOIN t_brands ON t_brands.idBrands = t_printers.idBrands
+      INNER JOIN t_prices ON t_prices.idPrinter = t_printers.idPrinter
+      ORDER BY t_prices.priDate DESC, t_brands.braName
+    `)
+    return result[0]
   }
 }
 
