3754452dedb27b5b5109c059de963e76671dc78c
vkaelin <kalanehd@gmail.com>
2019-12-10 09:27:08 +0100

    feat: DESIGN
    full caps

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      <span class="mx-2">|</span>
       <router-link to="/printers/1">Details</router-link>
     </div>
     <router-view />
